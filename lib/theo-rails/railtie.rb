93a7e1bac3a7514a3c9deaf5cc1370c6516c70bc
Jarek Lipski <loomchild@loomchild.net>
2025-08-03 15:53:52 +0200

    Register only .theo extension for templates, remove .erb.theo

--- lib/theo-rails/railtie.rb
+++ lib/theo-rails/railtie.rb
@@ -3,7 +3,7 @@ module Theo
     class Railtie < ::Rails::Railtie
       initializer 'initialize theo' do
         ActiveSupport.on_load(:action_view) do
-          ::ActionView::Template.register_template_handler :theo, :'erb.theo', Theo.new
+          ::ActionView::Template.register_template_handler :theo, Theo.new
           include ::Theo::Rails::Helpers
         end
       end
